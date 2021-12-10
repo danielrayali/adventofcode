99fdeedd49a040ab8bd44892630a8915c565254a
Daniel Ali <daniel.ray.ali@gmail.com>
2021-12-09 21:08:11 -0500

    Day 1 star 1 complete

--- 2021/d1s1.cc
+++ 2021/d1s1.cc
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <limits>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    cout << "Hello, World!" << endl;
+    ifstream in("d1s1.txt");
+    string line;
+    vector<int> data;
+    while (getline(in, line)) {
+        data.push_back(atoi(line.c_str()));
+    }
+
+    int prev = numeric_limits<int>::max();
+    int increases = 0;
+    for (int i = 0; i < data.size(); ++i) {
+        if (data.at(i) > prev) {
+            increases++;
+        }
+        prev = data.at(i);
+    }
+
+    cout << "Number of increases: " << increases << endl;
     return 0;
 }
