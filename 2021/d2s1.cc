195aa9bde72d04cf7703a228e7e88a962dabc809
Daniel Ali <daniel.ray.ali@gmail.com>
2021-12-18 11:59:56 -0500

    Initial D2S1

---
+++ 2021/d2s1.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    ifstream in("d2s1.txt");
+    string line;
+    vector<int> data;
+    int horizontal = 0, depth = 0;
+    while (getline(in, line)) {
+        if (line.empty()) {
+            continue;
+        }
+        size_t space = line.find_first_of(" ");
+        string command = line.substr(0, space);
+        int value = atoi(line.substr(space + 1).c_str());
+        cout << command << " " << value << endl;
+        if (command == "forward") {
+            horizontal += value;
+        } else if (command == "down") {
+            depth += value;
+        } else if (command == "up") {
+            depth -= value;
+        }
+    }
+
+    cout << "depth " << depth << endl;
+    cout << "horizontal " << horizontal << endl;
+    cout << "combined " << depth * horizontal << endl;
+    return 0;
+}
