54cee821e2d932495041f71a4b4c98d661e3f814
Daniel Ali <daniel.ray.ali@gmail.com>
2022-12-05 06:57:17 -0500

    Solved 2022 day 1 star 1

---
+++ 2022/d1s1/d1s1.rs
@@ -0,0 +1,22 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn main() {
+    let f = File::open("input.txt").expect("Unable to open file");
+    let reader = BufReader::new(f);
+    let mut max_calories = 0;
+    let mut elf_calories = 0;
+    for line_result in reader.lines() {
+        let line = line_result.unwrap();
+        if line.is_empty() {
+            if elf_calories > max_calories {
+                max_calories = elf_calories;
+            }
+            elf_calories = 0;
+        } else {
+            elf_calories += line.parse::<i32>().unwrap();
+        }
+    }
+    println!("Max calories: {}", max_calories);
+}
